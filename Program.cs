f06881df403cd4dfaa82c79eb5c07676636f52ac
its julii <nanji828@gmail.com>
2022-11-15 17:14:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            if (hour >= 10 && hour <= 18)
+            {
+                switch (day)
+                {
+                    case "Monday": break;
+                    case "Tuesday": break;
+                    case "Wednesday": break;
+                    case "Thursday": break;
+                    case "Friday": break;
+                    case "Saturday": break;
+                }
+            Console.WriteLine("open");
+                }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+            
+            
+                
+                
+
+
+
+            }
+        }
+    }
+
